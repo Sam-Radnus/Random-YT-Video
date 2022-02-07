53eee7f401ec2def98016515da91ecf61c54a2ff
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-02-07 22:55:08 +0530

    enabled random feature

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,15 +1,15 @@
 import React from 'react';
- async function componentDidMount()
- {
-    console.log("Hi");
- }
+import '../App.css';
+async function componentDidMount() {
+  console.log("Hi");
+}
 function Results(props) {
-    let{title,descpription,thumbnail}=props;
-  return <div>
-       <img src={thumbnail} onLoad={console.log("Hi") }alt={descpription}/>
-       <h1>{title}</h1>
+  let {link, title, descpription, thumbnail} = props;
+  return <div className="results">
+    <a  href={`https://www.youtube.com/watch?v=${link}`} ><img src={thumbnail} onLoad={console.log("Hi")} height="500" width="700" alt={descpription} /></a>
+    <h1 style={{textAlign:'center'}}>{title}</h1>
   </div>;
 }
 
 export default Results
-;
+  ;
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -6,6 +6,7 @@ let video=[];
 
 const  Search=(props)=>{
     let [video,getVideos]=useState([]);
+    
     const generateWord=async()=>{
         var determiner = ['able', ' bad', ' best', ' better', ' big', ' black',
             ' certain', ' clear', ' different', ' early', ' easy', ' economic', ' federal',
@@ -14,25 +15,21 @@ const  Search=(props)=>{
             ' long', ' low', ' major', ' military', ' national', ' new', ' old', ' only',
             ' other', ' political', ' possible', ' public', ' real', ' recent', ' right', ' small',
             ' social', ' special', ' strong', ' sure', ' true', ' white', ' whole', ' young  ',]
-    
+        
         var text = "";
-        let result = document.querySelector('div.result');
-        result.innerHTML="";  
+      
         //text += possible.charAt(Math.floor(Math.random() * possible.length));
         text = determiner[Math.floor(Math.random() * determiner.length)];
         var res = document.querySelector('input.form-control');
         res.value = text;
-       const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${text}&regionCode=GB&key=AIzaSyCSY-tbTAsPvUMEXPP32BGENmnG6nl5BzY`; 
+       const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${text}&regionCode=GB&key=AIzaSyCSY-tbTAsPvUMEXPP32BGENmnG6nl5BzY`; 
       let data = await fetch(url);
         let parsedData = await data.json();
-        getVideos(parsedData.items[0].snippet);
-       const resultVid = `<div>
-                       <img src=${video.thumbnails.high.url} onLoad={console.log("Hi") }alt=${video.descpription}/>
-                       <h1>${video.title}</h1>
-                     </div>`;
-       result.innerHTML+=resultVid;         
-        console.log(video);  
-    
+        console.log(parsedData);
+        video=parsedData.items;
+        console.log(video);
+        console.log(video[0].id.videoId);
+        getVideos(video);       
     }
     return <div className="SEARCH" >
         <h1 className="title">Random Youtube Video Generator</h1>
@@ -42,11 +39,11 @@ const  Search=(props)=>{
         </div>
 
         <button onClick={generateWord} type="button" className="btn btn-success">Generate Video</button>
-        <div onLoad={generateWord} className="result">
-         {/* {video.map((element)=>{
-                return <Results title={element.title} description={element.description} thumbnail={element.thumbnails.high.url}></Results>
-            })}  */}
-        </div>
+        
+          {video.map((element)=>{
+                return <Results key={element.id.videoId} link={element.id.videoId} title={element.snippet.title} description={element.snippet.description} thumbnail={element.snippet.thumbnails.high.url}></Results>
+            })}  
+       
     </div>;
 }
 Search.propTypes={
